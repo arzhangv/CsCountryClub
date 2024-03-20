81754b4024123f6e01798ff593a0c90d6239508e
SiddharthParatkar <siddharthparatkar@yahoo.com>
2024-03-20 00:00:40 -0400

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 const Home: React.FC = () => {
     return (
         <div className="bg-[url('/src/images/calabasas-country-club-home-photo.jpg')] h-96">
-            <div className="flex justify-center font-light text-3xl">The best for the best</div>
+            <div className="flex justify-center text-orange-600 text-3xl">The best for the best</div>
         </div>
     );
 };
