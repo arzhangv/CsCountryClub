111e8ac6ce86ab9cd4f30e48c599ae8e57547e2f
arzhangv <avaladkhani14@gmail.com>
2024-03-19 19:46:08 -0700

    fixing darths mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC =()=> {
               <nav>
                   <ul>
                       <li>
-                        <Link  to="/" style={{ display: 'flex', alignItems: 'center' }} >
+                        <Link  to="/" >
                             Home
                         </Link>
                       </li>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 
-import backgroundImage from './public/calabasas-country-club-home-photo.jpg'
+import country_club from '../images/calabasas-country-club-home-photo.jpg'
 import React from "react";
 const Home: React.FC = () => {
+
     return (
-        <div className="home">
-            <h1>Home Page</h1>
-            <p>This is the home page of our website.</p>
+
+        <div className="flex justify-items-center" >
+            <p> The best for the best </p>
         </div>
 
     );
