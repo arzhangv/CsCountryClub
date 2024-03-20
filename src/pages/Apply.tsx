e62129ba17a0426e46c6bcd0bb3baa376b1d8b97
SiddharthParatkar <siddharthparatkar@yahoo.com>
2024-03-19 23:41:58 -0400

    updating styling

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Apply() {
     return (
-        <div className="justify-items-center">
+        <div className="flex justify-center">
           <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLSehHyKrALx3qbZEtkflkvR9cY1pDmBd-L_v23ziqeX-k8Ii1A/viewform?embedded=true"
             width="640"
-            height="800"
+            height="1500"
             frameBorder="0"
             marginHeight={0}
             marginWidth={0}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,8 @@ const Home: React.FC = () => {
 
     return (
 
-        <div className="display-flex justify-center content-center bg-[url('/src/images/calabasas-country-club-home-photo.jpg')] h-96" >
-
-              <h2 className="justify-items-center"> The best for the best</h2>
-
+        <div className="bg-[url('/src/images/calabasas-country-club-home-photo.jpg')] h-96" >
+            <div className="flex justify-center font-light text-3xl">  The best for the best </div>
         </div>
 
     );
