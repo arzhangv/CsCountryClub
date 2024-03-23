ff36beb836e0d940154474c813eddaef9c20ebb7
kiajalali <kja54@sfu.ca>
2024-03-23 16:05:37 -0700

    suggestions for the project structure

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="relative overflow-hidden rounded-sm h-screen">
+      <div className="absolute inset-0 bg-[url('/src/assets/images/calabasas-country-club-home-photo.jpg')] bg-cover bg-center"></div>
+      <div className="bg-black/50 absolute inset-0"></div>
+      <div className="relative flex flex-col items-center justify-center h-full text-center p-4">
+        <h1 className="text-6xl text-white font-semibold mb-4">CS Country Club</h1>
+        <h4 className="text-4xl text-white font-semibold">The Best for the Best</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
