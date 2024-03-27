7bde2158b614deecf9611b6247b4676ba553ca41
arzhangv <avaladkhani14@gmail.com>
2024-03-27 00:13:54 -0700

    edited homepage style and added company logos

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -4,32 +4,32 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-green-900 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-semibold">
+      <div className="container mx-auto flex justify-between items-center ">
+        <div className=" font-semibold text-4xl">
           <Link to="/"> CS Country Club </Link>
         </div>
         <div className="space-x-4">
           <Link
             to="/"
-            className="hover:text-gold-500 transition-colors duration-200"
+            className="hover:text-gold-500 transition-colors duration-200 text-2xl"
           >
             Home
           </Link>
           <Link
             to="/mission"
-            className="hover:text-gold-500 transition-colors duration-200"
+            className="hover:text-gold-500 transition-colors duration-200 text-2xl"
           >
             Mission
           </Link>
           <Link
             to="/apply"
-            className="hover:text-gold-500 transition-colors duration-200"
+            className="hover:text-gold-500 transition-colors duration-200 text-2xl"
           >
             Apply
           </Link>
           <Link
             to="/about"
-            className="hover:text-gold-500 transition-colors duration-200"
+            className="hover:text-gold-500 transition-colors duration-200 text-2xl"
           >
             About
           </Link>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,73 @@
 import React from "react";
-
+import tesla_logo from "./tesla.png"
+import meta_logo from "./meta.png"
+import microsoft_logo from "./microsoft.png"
+import amazon_logo from "./amazon.png"
+import datadog_logo from "./datadog.png"
+import tiktok_logo from "./tiktok.png"
+import google_logo from "./google.png"
+import coinbase_logo from "./coinbase.png"
+import roblox_logo from "./roblox.png"
+import palantir_logo from "./palantir.png"
+import snowflake_logo from "./snowflake.png"
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col">
-      <div className="relative overflow-hidden rounded-sm h-screen">
-        <div className="absolute inset-0 bg-[url('/src/assets/images/calabasas-country-club-home-photo.jpg')] bg-cover bg-center"></div>
-        <div className="bg-black/50 absolute inset-0"></div>
-        <div className="relative flex flex-col items-center justify-center h-full text-center p-4">
-          <h1 className="text-6xl text-white font-semibold mb-4">CS Country Club</h1>
-          <h4 className="text-4xl text-white font-semibold">The Best for the Best</h4>
+        <div className="relative overflow-hidden rounded-sm h-screen">
+            <div
+                className="absolute inset-0 bg-[url('/src/assets/images/calabasas-country-club-home-photo.jpg')] h-1/2 bg-cover bg-center"></div>
+            <div className=" absolute inset-0"></div>
+            <div className="bg-black/50 relative flex flex-col items-center justify-center  text-center p-4 h-1/2">
+                <h1 className="text-6xl text-white font-semibold mb-4">CS Country Club</h1>
+                <h4 className="text-4xl text-white font-semibold ">The Best for the Best</h4>
+            </div>
+            <div className="bg-white/10 text-3xl h-1/2 flex flex-col items-center justify-center">
+                <h1 className="p-8"> Join an active community with members from top companies </h1>
+
+                <div className="flex flex-row items-center justify-center">
+                    <div className="h-28 w-34 ">
+                        <img src={meta_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-24 w-28 p-2">
+                        <img src={tesla_logo}/>
+                    </div>
+                    <div className="h-28 w-36 p-2">
+                        <img src={microsoft_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-28 w-34 p-2">
+                        <img src={amazon_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-24 w-24 p-2">
+                        <img src={datadog_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-24 w-28 p-2">
+                        <img src={tiktok_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+
+                </div>
+                <div className="flex flex-row items-center justify-center ">
+                    <div className="h-28 w-30 p-4">
+                        <img src={google_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-24 w-24 p-2">
+                        <img src={coinbase_logo}/>
+                    </div>
+                    <div className="h-28 w-36 p-2">
+                        <img src={roblox_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-28 w-36 p-2">
+                        <img src={palantir_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-24 w-24 p-2 ">
+                        <img src={snowflake_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+                    <div className="h-24 w-24 p-2">
+                        <img src={tesla_logo} className="object-cover object-center h-full w-full"/>
+                    </div>
+
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 };
