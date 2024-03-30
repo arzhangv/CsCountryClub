cbe105cdb7a4772d8dcb65d56255e69582e6eb88
arzhangv <avaladkhani14@gmail.com>
2024-03-30 13:56:56 -0700

    added bbg

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,8 +8,9 @@ import tiktok_logo from "../../assets/images/logos/tiktok.png"
 import google_logo from "../../assets/images/logos/google.png"
 import coinbase_logo from "../../assets/images/logos/coinbase.png"
 import roblox_logo from "../../assets/images/logos/roblox.png"
-import palantir_logo from "../../assets/images/logos/palantir.png"
-import snowflake_logo from "../../assets/images/logos/snowflake.png"
+import palantir_logo from "../../assets/images/logos/palantir2.png"
+import snowflake_logo from "../../assets/images/logos/snowflake2.png"
+import bloomberg from "../../assets/images/logos/bloomberg.png"
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col">
@@ -25,7 +26,7 @@ const Home: React.FC = () => {
                 <h1 className="p-8"> Join an active community with members from top companies </h1>
 
                 <div className="flex flex-row items-center justify-center">
-                    <div className="h-28 w-34 ">
+                    <div className="h-26 w-32 ">
                         <img src={meta_logo} className="object-cover object-center h-full w-full"/>
                     </div>
                     <div className="h-24 w-28 p-2">
@@ -46,23 +47,29 @@ const Home: React.FC = () => {
 
                 </div>
                 <div className="flex flex-row items-center justify-center ">
-                    <div className="h-28 w-30 p-4">
+                    <div className="h-24 w-24 p-4">
                         <img src={google_logo} className="object-cover object-center h-full w-full"/>
                     </div>
+                    <div className="w-3 p-1"></div>
                     <div className="h-24 w-24 p-2">
                         <img src={coinbase_logo}/>
                     </div>
+                    <div className="w-3 p-1"></div>
+
                     <div className="h-28 w-36 p-2">
                         <img src={roblox_logo} className="object-cover object-center h-full w-full"/>
                     </div>
-                    <div className="h-28 w-36 p-2">
+
+                    <div className="h-22 w-24 p-4">
                         <img src={palantir_logo} className="object-cover object-center h-full w-full"/>
                     </div>
+
                     <div className="h-24 w-24 p-2 ">
                         <img src={snowflake_logo} className="object-cover object-center h-full w-full"/>
                     </div>
-                    <div className="h-24 w-24 p-2">
-                        <img src={tesla_logo} className="object-cover object-center h-full w-full"/>
+
+                    <div className="h-30 w-28 p-4">
+                        <img src={bloomberg} className="object-cover object-center h-full w-full"/>
                     </div>
 
                 </div>
